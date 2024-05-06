d9523878db700152adfc5187d3456cc8f5da4c04
Sachin naik <ksachinnaik5253@gmail.com>
2024-05-06 15:32:03 +0530

    delte old files for restructure the app

--- src/components/ApplyButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-
-const ApplyButton = () => {
-  return <Button variant="contained" color="primary" style={{ marginTop: 10 }}>Apply</Button>;
-};
-
-export default ApplyButton;
--- src/components/Experience.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-
-const Experience = ({ experience }) => {
-  return <Typography color="textSecondary" style={{ marginTop: 10 }}>Experience: {experience}</Typography>;
-};
-
-export default Experience;
